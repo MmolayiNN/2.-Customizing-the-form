6b84dab532d312cc9fa119ad07f129751e8532c0
Hohm <Hohm@HOHM>
2024-01-19 13:33:31 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("this is Nelca", , " Greetings, I hope you had a greate day")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+
+    End Sub
+End Class
